d8cfa58568eb527147118a57bf0963c946ff91c1
frankwei <1183806196@qq.com>
2020-11-22 00:24:20 +0800

    deploy

---
+++ precache-manifest.df18db73070588fa5c7287b2acd8423d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3141da0fbfaf0f30d848",
+    "url": "/accounts-website/css/app.dcbc2b29.css"
+  },
+  {
+    "revision": "1c44f19db396f8e03701e5960bbf3589",
+    "url": "/accounts-website/index.html"
+  },
+  {
+    "revision": "3141da0fbfaf0f30d848",
+    "url": "/accounts-website/js/app.4fe7dcfc.js"
+  },
+  {
+    "revision": "29c63449cb7e11f2bd9f",
+    "url": "/accounts-website/js/chunk-vendors.fe6d507c.js"
+  },
+  {
+    "revision": "8743e3c6d3a9deb93a5a625838948152",
+    "url": "/accounts-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/accounts-website/robots.txt"
+  }
+]);
